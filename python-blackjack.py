e4bbe1ae699d3c2e3de49d05281674c1ecd6e5e0
Martin Foakes <foakes.m@gmail.com>
2020-02-18 14:40:11 +0100

    Update python-blackjack.py

--- python-blackjack.py
+++ python-blackjack.py
@@ -91,10 +91,13 @@ class Hand:
     def add_card(self,card):
         self.cards.append(card)
         self.value += values[card.rank]
+        # If the card is an Ace, amend aces attribute
         if card.rank == 'Ace':
             self.aces += 1  # add to self.aces
 
     def adjust_for_ace(self):
+        # If hand value is over 21, and self.aces == 1, amend
+        # value and reset self.aces (== 0)
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1 
